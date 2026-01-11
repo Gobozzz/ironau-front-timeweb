c3dfaa52405cb74b46a7960cc7caa2c681243f10
Gobozzz <gobozovbogdan@gmail.com>
2026-01-11 10:25:29 +0300

    32 commit

--- app/constants.ts
+++ app/constants.ts
@@ -1,5 +1,5 @@
-// export const API_URL = "https://api.front-test.ru/api";
-export const API_URL = "http://localhost:8000/api";
+export const API_URL = "https://api.front-test.ru/api";
+// export const API_URL = "http://localhost:8000/api";
 export const SITE_NAME = "Ironau";
 export const SITE_URL = "https://ironau.ru";
 export const SUPPORT_EMAIL = "support@ironau.ru";
